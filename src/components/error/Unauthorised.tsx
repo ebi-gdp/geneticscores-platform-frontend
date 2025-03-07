9baae8f1b2c0aa4854b90764f143ff8b3f0a31be
Ashutosh <ashutosh@ebi.ac.uk>
2025-03-07 14:47:14 +0000

    - Minor text change - Session codition check on unauthorized page

--- src/components/error/Unauthorised.tsx
+++ src/components/error/Unauthorised.tsx
@@ -1,28 +1,36 @@
 import {useAuth} from "../../auth/UserProvider";
+import {Navigate} from "react-router-dom";
+import {PGS_CALCULATOR} from "../../util/URIConstants";
+import React from "react";
 
 export const Unauthorised = () => {
-    const {login} = useAuth();
-    return (
-        <>
-            <div className="overlay-unauthorized">
-                <div className="unauthorized-container">
-                    <table className="message">
-                        <tbody>
-                        <tr>
-                            <td><h2>You're currently not signed in</h2></td>
-                        </tr>
-                        <tr>
-                            <td className="text-center-alignment">
-                                <button className="vf-button vf-button--primary vf-button--sm"
-                                        onClick={() => login()}>Log
-                                    in
-                                </button>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+    const {isAuthenticated, login} = useAuth();
+
+    if (isAuthenticated) {
+        return <Navigate to={PGS_CALCULATOR}/>;
+    } else {
+        return (
+            <>
+                <div className="overlay-unauthorized">
+                    <div className="unauthorized-container">
+                        <table className="message">
+                            <tbody>
+                            <tr>
+                                <td><h2>You're currently not signed in</h2></td>
+                            </tr>
+                            <tr>
+                                <td className="text-center-alignment">
+                                    <button className="vf-button vf-button--primary vf-button--sm"
+                                            onClick={() => login()}>Log
+                                        in
+                                    </button>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-        </>
-    );
+            </>
+        );
+    }
 }
