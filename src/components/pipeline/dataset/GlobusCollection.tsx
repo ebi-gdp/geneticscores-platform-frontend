3b64eb6eb8572eff9f7439e025b40d209fe273b2
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2025-03-12 15:10:21 +0000

    add globus links

--- src/components/pipeline/dataset/GlobusCollection.tsx
+++ src/components/pipeline/dataset/GlobusCollection.tsx
@@ -9,9 +9,15 @@ export const GlobusCollection = ({previousStep, nextStep}: CombinedStepAction) =
             <h1>Globus data transfer</h1>
             <p className="vf-content__standsecond">
                 <h4>We use Globus to securely transfer data.</h4></p>
-            <p className="vf-text-body vf-text-body--3">You need to create a Globus account to upload your data.
+            <p className="vf-text-body vf-text-body--3">You need to create a <a href="https://www.globus.org"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            className="vf-link">Globus</a> account to upload your data.
                 Keep track of the Globus account identity (email address) you use to create your account.</p>
-            <p className="vf-text-body vf-text-body--3">You can upload data &lt; 15 GB via a Globus web browser.</p>
+            <p className="vf-text-body vf-text-body--3">You can upload data &lt; 15 GB via <a href="https://app.globus.org/file-manager"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            className="vf-link">a Globus web browser.</a></p>
             <p className="vf-text-body vf-text-body--3">If you want to transfer data &gt; 15 GB from your computer
                 you will need to install <a href={DOCS_URL}
                                             target="_blank"
